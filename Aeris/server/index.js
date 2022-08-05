1fc597271db7d7b073bf95b510ab793c0de79f80
PatelAerisM <93403587+PatelAerisM@users.noreply.github.com>
2022-08-05 22:30:49 +0530

    First Commit

---
+++ Aeris/server/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGO_URL)
+  .then(() => console.log('MongoDB is connected'))
+  .catch((err) => { console.log(err) });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server started on ${5000}`)
+);
+
+
+const io = socket(server, {
+  cors: {
+    origin: "https://whimsical-dolphin-08c677.netlify.app",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
